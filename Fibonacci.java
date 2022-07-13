b738a0c74736762c51594f086431dd4ef354ea56
Alia <alimmeek@DESKTOP-H6M3AFB.localdomain>
2022-07-13 17:36:32 +0200

    Added Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,29 @@
+public class Fibonacci {
+	public static void main(String[] args) {
+		int n = parseArgs(args);
+		if (n == -1) {
+			System.err.println("Invalid arguments");
+		}
+		FibonacciCalculator fC = new FibonacciCalculator();
+		for (int i = 0; i < n-1; i++) {
+			System.out.print(fC.calculate(i) + ", ");
+		}
+		System.out.println(fC.calculate(n-1));
+	}
+
+	private static Integer parseArgs(String[] args) {
+		if (args.length != 1 || args[0] == null) {
+			return -1;
+		}
+		int n;
+		try {
+			n = Integer.parseInt(args[0]);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+		if (n < 0) {
+			return -1;
+		}
+		return n;
+	}
+}
